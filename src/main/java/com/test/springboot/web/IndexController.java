f9c894bd0f8c11a6b8f550076acc3f136307a13b
wndms0901 <cherry25987@gmail.com>
2021-02-27 00:00:04 +0900

    210226 5장 (p.195~223) - 세션값 가져오는 부분을 어노테이션으로 개선 - 세션 저장소로 데이터베이스 사용 - 네이버 로그인 추가 - 테스트에 시큐리티 적용

--- src/main/java/com/test/springboot/web/IndexController.java
+++ src/main/java/com/test/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.test.springboot.web;
 
+import com.test.springboot.config.auth.LoginUser;
 import com.test.springboot.config.auth.dto.SessionUser;
 import com.test.springboot.service.PostsService;
 import com.test.springboot.web.dto.PostsResponseDto;
@@ -20,9 +21,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        // SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
